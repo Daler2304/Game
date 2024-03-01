ffa2370df81a4a08745ec82e2857522ba6bb8baa
Daler2304 <110163963+Daler2304@users.noreply.github.com>
2024-03-01 13:07:07 +0300

    Update bullet.h

--- bullet.h
+++ bullet.h
@@ -1,12 +1,10 @@
 #pragma once
 
 #include "Location.h"
-#include <ctime>
 
 class Bullet : public Location {
 private:
     char bullet = '*';
-    time_t bulletTime;
 
 public:
     char getBullet() const;
